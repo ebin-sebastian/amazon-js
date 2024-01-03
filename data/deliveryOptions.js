584f391b894fe3aebb36f6c36316fa2aeaacad38
Ebin Sebastian <ebinsebatian888@gmail.com>
2024-01-03 19:41:08 +0530

    save data and generate HTML for payment summary

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -12,3 +12,13 @@ export const deliveryOptions=[{
     priceCents:999
 }
 ];
+
+export function getDeliveryOption(deliveryOptionId){
+    let deliveryOption;
+        deliveryOptions.forEach((option) => {
+          if (deliveryOptionId === option.id) {
+            deliveryOption = option;
+          }
+        });
+    return deliveryOption;
+}
--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -5,10 +5,10 @@ import {
     updateQuantity,
     updateDeliveryOption
   } from "../../data/cart.js";
-  import { products } from "../../data/products.js";
+  import { products,getProduct} from "../../data/products.js";
   import { formatCurrency } from "../utils/money.js";
   import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
-  import { deliveryOptions } from "../../data/deliveryOptions.js";
+  import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
   
   
   export function renderOrderSummary(){
@@ -19,22 +19,14 @@ import {
       cart.forEach((cartItem) => {
         const productId = cartItem.productId;
   
-        let matchingProduct;
+        const matchingProduct=getProduct(productId);
   
-        products.forEach((product) => {
-          if (product.id === productId) {
-            matchingProduct = product;
-          }
-        });
+        
   
         const deliveryOptionId = cartItem.deliveryOptionId;
   
-        let deliveryOption;
-        deliveryOptions.forEach((option) => {
-          if (deliveryOptionId === option.id) {
-            deliveryOption = option;
-          }
-        });
+        const deliveryOption=getDeliveryOption(deliveryOptionId);
+        
         const today = dayjs();
         const deliveryDate = today.add(deliveryOption.deliveryDays, "days");
         const dateString = deliveryDate.format("dddd, MMMM D");
