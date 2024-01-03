53077059f87180d3f4daf77be0a285d4837bb2e2
Ebin Sebastian <ebinsebatian888@gmail.com>
2024-01-03 14:58:08 +0530

    make delivery options interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,6 +3,7 @@ import {
   removeFromCart,
   calculateCartQuantity,
   updateQuantity,
+  updateDeliveryOption
 } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
@@ -105,7 +106,10 @@ function deliveryOptionsHtml(matchingProduct, cartItem) {
     const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
 
     html += `
-                <div class="delivery-option">
+                <div class="delivery-option js-delivery-option" 
+                data-product-id="${matchingProduct.id}"
+                data-delivery-option-id="${deliveryOption.id}"
+                >
                   <input type="radio" ${
                     isChecked ? "Checked" : ""
                   } class="delivery-option-input"
@@ -182,3 +186,12 @@ function updateCartQuantity() {
 }
 
 updateCartQuantity();
+
+document.querySelectorAll('.js-delivery-option').forEach((element)=>{
+  element.addEventListener("click",()=>{
+    const {productId,deliveryOptionId}=element.dataset;
+    updateDeliveryOption(productId,deliveryOptionId)
+  })
+})
+
+
